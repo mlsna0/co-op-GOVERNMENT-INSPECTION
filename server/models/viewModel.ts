22dc3eadaca8fa4ec7c0316683ef419ff2b06e32
Suphawict STS <mlsna.1209na@gmail.com>
2024-06-19 14:07:37 +0700

    ppp

--- server/models/viewModel.ts
+++ server/models/viewModel.ts
@@ -2,7 +2,8 @@ import * as mongoose from 'mongoose';
 
 const viewSchema = new mongoose.Schema({
     view_rank: String,
-    view_full_name: String,
+    view_first_name: String,
+    view_last_name: String,
 
     RecordModelId: { type: mongoose.Schema.Types.ObjectId, ref: 'recordModel' }
 });
