c7f719bd35a9c3c6512467cf71386b41476d8c91
Weerapat <64310097@go.buu.ac.th>
2024-06-20 09:09:16 +0700

    20//06 sidebar edit

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,6 +10,7 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
    
     { path: '/table-list', title: 'การลงตรวจอิเล็กทรอนิค',  icon:'content_paste', class: '' },
+    // { path: '/table-detail/:id', title: 'การลงตรวจอิเล็กทรอนิค',  icon:'content_paste', class: '' },
     // { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     // { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
     // { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
