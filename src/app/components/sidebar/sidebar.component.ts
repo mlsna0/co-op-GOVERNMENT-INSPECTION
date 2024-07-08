2c5bd9b19afced73b35f99b1d851425a61aadf33
Weerapat <64310097@go.buu.ac.th>
2024-07-08 15:38:45 +0700

    comment

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -16,12 +16,12 @@ export const ROUTES: RouteInfo[] = [
     { path: '/table-main', title: 'การลงตรวจอิเล็กทรอนิค',  icon:'content_paste', class: '' },
     { path: '/reportuser', title: 'รายงานสมัครเข้าใช้งาน' , icon:'person', class:''},
     { path: '/map', title: 'แผนที่ประเทศไทย', icon: 'map', class:''},
-    { path: '/reportuser', title: 'รายงานสมัครเข้าใช้งาน' , icon:'person', class:'',
-      children: [
-        { path: '/reportuser/reportuser', title: 'Sub Report 1', icon: 'person', class: '' },
-        { path: '/reportuser/reportprofile', title: 'Sub Report 2', icon: 'person', class: '' }
-      ]
-    },
+    // { path: '/reportuser', title: 'รายงานสมัครเข้าใช้งาน' , icon:'person', class:'',
+    //   children: [
+    //     { path: '/reportuser/reportuser', title: 'Sub Report 1', icon: 'person', class: '' },
+    //     { path: '/reportuser/reportprofile', title: 'Sub Report 2', icon: 'person', class: '' }
+    //   ]
+    // },
     // { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
     // { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
     // { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
